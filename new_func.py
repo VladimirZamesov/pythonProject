960bc5c9561d9e579b468796b77de2fa60b8eeec
VladimirZamesov <112873709+VladimirZamesov@users.noreply.github.com>
2023-01-19 12:07:12 +0300

    Update new_func.py

--- new_func.py
+++ new_func.py
@@ -1,7 +1,4 @@
 def get_result(user_param, d):
     for i, x in enumerate(d):
-        if i == len(d) - 1:
-            return d['last']
-        elif user_param in x:
-            return d[x]
+        return d[x] if user_param in d else d['last']
 
